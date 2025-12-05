13a85810342f4baf71b24a64d9932f0c65eaa3a1
erikproper <erikproper@gmail.com>
2025-12-05 07:52:40 +0100

    $1

--- connect/layer_3_coordination.go
+++ connect/layer_3_coordination.go
@@ -1,18 +1,19 @@
 // /*
-//  *
-//  * Package: mbconnect
-//  * Layer:   3
-//  * Module:  json_artefacts
-//  *
-//  * ..... ... .. .
-//  *
-//  * Creator: Henderik A. Proper (e.proper@acm.org), TU Wien, Austria
-//  *
-//  * Version of: XX.11.2025
-//  *
-//  */
 //
-// package connect
+//	*
+//	* Package: mbconnect
+//	* Layer:   3
+//	* Module:  json_artefacts
+//	*
+//	* ..... ... .. .
+//	*
+//	* Creator: Henderik A. Proper (e.proper@acm.org), TU Wien, Austria
+//	*
+//	* Version of: XX.11.2025
+//	*
+//	*/
+package connect
+
 //
 // import (
 // 	"encoding/json"
