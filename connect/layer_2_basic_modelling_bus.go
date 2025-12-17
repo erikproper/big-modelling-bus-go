a702073dc9faa9ef25ff9acd1a08c2c509eb04bb
erikproper <erikproper@gmail.com>
2025-12-17 22:28:43 +0100

    $1

--- connect/layer_2_basic_modelling_bus.go
+++ connect/layer_2_basic_modelling_bus.go
@@ -74,6 +74,8 @@ func (b *TModellingBusConnector) postJSONAsFile(topicPath string, jsonMessage []
 	// First, add the JSON as a file to the repository
 	event := b.modellingBusRepositoryConnector.addJSONAsFile(topicPath, jsonMessage, timestamp)
 
+	b.Reporter.Progress(1, "JJJJ")
+
 	// Then convert the event to JSON
 	message, err := json.Marshal(event)
 
